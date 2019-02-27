8fbd2d5c43c796b5abd2bb4672635e67de5d7d9f
Samuel Rocha <samuelr9>
2019-02-27 19:18:31 -0300

    simplificação do código

--- doom_fire_animation.py
+++ doom_fire_animation.py
@@ -4,18 +4,18 @@ Created on Tue Feb 26 23:21:19 2019
 @author: Samuel Rocha
 """
 
-import numpy as np
-import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
+import matplotlib.pyplot as plt
+import numpy as np
 
 class Animation(object):
     def __init__(self):
-        self.res = (80,50)
-        self.initAnimationData()
+        self.res = (100,100)
         self.fig = plt.figure("Doom Fire",figsize=self.res)
-        self.axe = self.fig.add_axes([0, 0, 1, 1], frameon=True)
+        self.axe = self.fig.add_axes([0, 0, 1, 1], frameon=False)
         self.axe.set_xlim(0.5, self.res[1]-0.5), self.axe.set_xticks([])
         self.axe.set_ylim(0.5,self.res[0]-0.5), self.axe.set_yticks([])
+        self.initAnimationData()
         
     def runAnimation(self):
         self.renderAnimation()
@@ -27,18 +27,16 @@ class Animation(object):
 
     def initAnimationData(self):
         np.random.seed(0)
-        self.fire_min = 0
-        self.fire_max = self.res[0]
+        self.fire_min, self.fire_max = 0, self.res[0]
         self.doom_fire = np.zeros(self.res,dtype=np.float32)
         self.doom_fire[0] = self.fire_max
 
     def updateAnimationData(self,frame):
         for i in range(self.res[0]-1,0,-1):
-            fire_decay = np.zeros((1,self.res[1]),dtype=np.float32)
             for j in range(0,self.res[1]):
-                fire_decay[0][j] = 3*np.random.random(1)[0]
-                move_flame = np.random.randint(0,3)
-                self.doom_fire[i][j-move_flame] = self.doom_fire[i-1][j] - fire_decay[0][j]
+                fire_decay = 3*np.random.random(1)[0]
+                wind = np.random.randint(0,3)
+                self.doom_fire[i][j-wind] = self.doom_fire[i-1][j] - fire_decay
         self.doom_fire_render.set_data(self.doom_fire)
                 
 if __name__ == '__main__':
